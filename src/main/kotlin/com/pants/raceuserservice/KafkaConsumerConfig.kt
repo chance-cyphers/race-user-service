1a681bf9663bbca20a01c31b05db189a3aa48402
chance.a.cyphers <chance.a.cyphers@gmail.com>
2019-05-06 12:47:46 -0400

    Switch to spring kafka

---
+++ src/main/kotlin/com/pants/raceuserservice/KafkaConsumerConfig.kt
@@ -0,0 +1,43 @@
+package com.pants.raceuserservice
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+
+
+@EnableKafka
+@Configuration
+class KafkaConsumerConfig {
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, String> {
+        val brokers = System.getenv("CLOUDKARAFKA_BROKERS")
+        val username = System.getenv("CLOUDKARAFKA_USERNAME")
+        val password = System.getenv("CLOUDKARAFKA_PASSWORD")
+        val jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";"
+        val jaasCfg = String.format(jaasTemplate, username, password)
+
+        val props = hashMapOf<String, Any>(
+                "bootstrap.servers" to brokers,
+                "key.deserializer" to "org.apache.kafka.common.serialization.StringDeserializer",
+                "value.deserializer" to "org.apache.kafka.common.serialization.StringDeserializer",
+                "group.id" to "$username-consumer",
+                "security.protocol" to "SASL_SSL",
+                "sasl.mechanism" to "SCRAM-SHA-256",
+                "sasl.jaas.config" to jaasCfg
+        )
+
+        return DefaultKafkaConsumerFactory(props)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String> {
+        return ConcurrentKafkaListenerContainerFactory<String, String>().apply {
+            consumerFactory = consumerFactory()
+        }
+    }
+
+}
--- src/main/kotlin/com/pants/raceuserservice/WinsController.kt
+++ src/main/kotlin/com/pants/raceuserservice/WinsController.kt
@@ -1,22 +1,24 @@
 package com.pants.raceuserservice
 
-import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.Duration
-import java.util.*
-import kotlin.concurrent.thread
 
 @RestController
 class WinsController {
 
     var counter = 0
 
-    init {
-        thread(start = true) {
-            println("starting with the consumption")
-            startConsuming()
-        }
+//    init {
+//        thread(start = true) {
+//            println("starting with the consumption")
+//            startConsuming()
+//        }
+//    }
+
+    @KafkaListener(topics = ["tuh5qbtu-default"])
+    fun listen() {
+        counter++
     }
 
     @RequestMapping("/")
@@ -24,40 +26,39 @@ class WinsController {
         return "Race api was visited $counter times, lately"
     }
 
-    fun startConsuming() {
-        val brokers = System.getenv("CLOUDKARAFKA_BROKERS")
-        val username = System.getenv("CLOUDKARAFKA_USERNAME")
-        val password = System.getenv("CLOUDKARAFKA_PASSWORD")
-        val topic = "$username-default"
-
-        val consumer = createConsumer(brokers, username, password)
-        consumer.subscribe(Arrays.asList(topic))
-
-        while (true) {
-            val records = consumer.poll(Duration.ofMillis(1000))
-            records.forEach {
-                println("got a record: " + it.value())
-                counter++
-            }
-        }
-    }
+//    fun startConsuming() {
+//        val brokers = System.getenv("CLOUDKARAFKA_BROKERS")
+//        val username = System.getenv("CLOUDKARAFKA_USERNAME")
+//        val password = System.getenv("CLOUDKARAFKA_PASSWORD")
+//        val topic = "$username-default"
+//
+//        val consumer = createConsumer(brokers, username, password)
+//        consumer.subscribe(Arrays.asList(topic))
+//
+//        while (true) {
+//            val records = consumer.poll(Duration.ofMillis(1000))
+//            records.forEach {
+//                println("got a record: " + it.value())
+//                counter++
+//            }
+//        }
+//    }
 
 }
-
-private fun createConsumer(brokers: String, username: String, password: String): KafkaConsumer<String, String> {
-    val jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";"
-    val jaasCfg = String.format(jaasTemplate, username, password)
-
-    val props = Properties()
-
-    props["bootstrap.servers"] = brokers
-    props["key.deserializer"] = "org.apache.kafka.common.serialization.StringDeserializer"
-    props["value.deserializer"] = "org.apache.kafka.common.serialization.StringDeserializer"
-    props["group.id"] = "$username-consumer"
-
-    props["security.protocol"] = "SASL_SSL"
-    props["sasl.mechanism"] = "SCRAM-SHA-256"
-    props["sasl.jaas.config"] = jaasCfg
-
-    return KafkaConsumer(props)
-}
+//
+//private fun createConsumer(brokers: String, username: String, password: String): KafkaConsumer<String, String> {
+//    val jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";"
+//    val jaasCfg = String.format(jaasTemplate, username, password)
+//
+//    val props = Properties()
+//
+//    props["bootstrap.servers"] = brokers
+//    props["key.deserializer"] = "org.apache.kafka.common.serialization.StringDeserializer"
+//    props["value.deserializer"] = "org.apache.kafka.common.serialization.StringDeserializer"
+//    props["group.id"] = "$username-consumer"
+//    props["security.protocol"] = "SASL_SSL"
+//    props["sasl.mechanism"] = "SCRAM-SHA-256"
+//    props["sasl.jaas.config"] = jaasCfg
+//
+//    return KafkaConsumer(props)
+//}
