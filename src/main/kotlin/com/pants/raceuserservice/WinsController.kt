fa7aa82382c13b5a5256817ae35a4714c809b0eb
chance.a.cyphers <chance.a.cyphers@gmail.com>
2019-05-06 18:01:27 -0400

    Try some db stuff

--- src/main/kotlin/com/pants/raceuserservice/WinsController.kt
+++ src/main/kotlin/com/pants/raceuserservice/WinsController.kt
@@ -1,12 +1,14 @@
 package com.pants.raceuserservice
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.sql.DataSource
 
 
 @RestController
-class WinsController {
+class WinsController(@Autowired val dataSource: DataSource) {
 
     var counter = 0
 
@@ -20,11 +22,26 @@ class WinsController {
         return "Race api was visited $counter times, lately"
     }
 
-    @RequestMapping("/db-test")
-    fun count(): String {
-        return "sd"
+    @RequestMapping("/db")
+    fun db(): List<String> {
+        try {
+            dataSource.connection.use { connection ->
+                val stmt = connection.createStatement().apply {
+                    executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
+                    executeUpdate("INSERT INTO ticks VALUES (now())")
+                }
+                val rs = stmt.executeQuery("SELECT tick FROM ticks")
+
+                val output = mutableListOf<String>()
+                while (rs.next()) {
+                    output.add("Read from DB: " + rs.getTimestamp("tick"))
+                }
+
+                return output.toList()
+            }
+        } catch (e: Exception) {
+            return mutableListOf("error")
+        }
     }
-
-
 
 }
