f6bee232db2a3331308b0b0328318978e816a828
chance.a.cyphers <chance.a.cyphers@gmail.com>
2019-05-06 12:56:42 -0400

    Cleanup

--- src/main/kotlin/com/pants/raceuserservice/WinsController.kt
+++ src/main/kotlin/com/pants/raceuserservice/WinsController.kt
@@ -9,13 +9,6 @@ class WinsController {
 
     var counter = 0
 
-//    init {
-//        thread(start = true) {
-//            println("starting with the consumption")
-//            startConsuming()
-//        }
-//    }
-
     @KafkaListener(topics = ["tuh5qbtu-default"])
     fun listen() {
         counter++
@@ -26,39 +19,4 @@ class WinsController {
         return "Race api was visited $counter times, lately"
     }
 
-//    fun startConsuming() {
-//        val brokers = System.getenv("CLOUDKARAFKA_BROKERS")
-//        val username = System.getenv("CLOUDKARAFKA_USERNAME")
-//        val password = System.getenv("CLOUDKARAFKA_PASSWORD")
-//        val topic = "$username-default"
-//
-//        val consumer = createConsumer(brokers, username, password)
-//        consumer.subscribe(Arrays.asList(topic))
-//
-//        while (true) {
-//            val records = consumer.poll(Duration.ofMillis(1000))
-//            records.forEach {
-//                println("got a record: " + it.value())
-//                counter++
-//            }
-//        }
-//    }
-
 }
-//
-//private fun createConsumer(brokers: String, username: String, password: String): KafkaConsumer<String, String> {
-//    val jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";"
-//    val jaasCfg = String.format(jaasTemplate, username, password)
-//
-//    val props = Properties()
-//
-//    props["bootstrap.servers"] = brokers
-//    props["key.deserializer"] = "org.apache.kafka.common.serialization.StringDeserializer"
-//    props["value.deserializer"] = "org.apache.kafka.common.serialization.StringDeserializer"
-//    props["group.id"] = "$username-consumer"
-//    props["security.protocol"] = "SASL_SSL"
-//    props["sasl.mechanism"] = "SCRAM-SHA-256"
-//    props["sasl.jaas.config"] = jaasCfg
-//
-//    return KafkaConsumer(props)
-//}
